53a0146ef32d167029d5612302383eaf9238e161
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-01-06 22:37:06 -0300

    Minor changes.

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,18 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
-  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-  "x-rapidapi-key": "3b4c2a6edfmshbf62cbc666ace44p111ab9jsn9fbce1aa42be",
+  "x-rapidapi-host": process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+  "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
 };
-
-const baseUrl = "https://coinranking1.p.rapidapi.com"
 
 const createRequest = (url) => ({url, headers: cryptoApiHeaders})
 
 export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
     baseQuery: fetchBaseQuery({
-        baseUrl
+        baseUrl: process.env.REACT_APP_CRYPTO_API_URL
     }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -2,18 +2,16 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoNewsHeaders = {
     'x-bingapis-sdk': 'true',
-    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
-    'x-rapidapi-key': '3b4c2a6edfmshbf62cbc666ace44p111ab9jsn9fbce1aa42be'
+    'x-rapidapi-host': process.env.REACT_APP_NEWS_RAPIDAPI_HOST,
+    'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY
 };
-
-const baseUrl = "https://bing-news-search1.p.rapidapi.com"
 
 const createRequest = (url) => ({url, headers: cryptoNewsHeaders})
 
 export const cryptoNewsApi = createApi({
     reducerPath: 'cryptoNewsApi',
     baseQuery: fetchBaseQuery({
-        baseUrl
+        baseUrl: process.env.REACT_APP_NEWS_RAPIDAPI_HOST
     }),
     endpoints: (builder) => ({
         getCryptoNews: builder.query({
