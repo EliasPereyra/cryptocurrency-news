ff8d4e93e96fd465fd8671a85ed795b05b423618
EliasPereyra <EliasPereyra_Gomez@hotmail.com>
2022-01-08 23:46:52 -0300

    Minor changes.

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,4 +1,21 @@
+import { useState } from 'react'
+import HTMLReactParses from 'html-react-parser'
+import { useParams } from 'react-router-dom'
+
+import millify from 'millify'
+import { Col, Row, Typography, Select } from 'antd'
+import { MoneyCollectOutlined, DollarCircleOutlined, FoundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons'
+import { useGetCryptoDetailsQuery } from '../services/cryptoApi'
+
+
+const { Title, Text} = Typography
+const { Option } = Select
+
 const CryptoDetails = () => {
+    const { coinId } = useParams()
+    const [timePeriod, setTimePeriod] = useState('7d')
+    const {data, isFetching } = useGetCryptoDetailsQuery(coinId)
+
     return (
         <div>
             
