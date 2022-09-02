d6b58672818dc6ee801d21b62ba77aafd690ffc4
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2022-09-01 22:05:49 -0300

    Adding a logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,69 @@
-import { useState, useEffect } from 'react';
-import { Button, Menu, Typography, Avatar } from 'antd'
-import { Link } from 'react-router-dom'
-import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
+import { useState, useEffect } from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
 
-import icon from '../images/cryptocurrency.png'
+import Digcoin from "../assets/Digcoin.png";
 
 const Navbar = () => {
-    const [activeMenu, setActiveMenu] = useState(true)
-    const [screenSize, setScreenSize] = useState(null)
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
 
-    useEffect(() => {
-        const handleResize = () => setScreenSize(window.innerWidth)
-        window.addEventListener('resize', handleResize)
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
 
-        handleResize()
-        return () => window.removeEventListener('resize', handleResize)
-    }, [])
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-    useEffect(() => {
-        if (screenSize < 768) {
-            setActiveMenu(false)
-        }else {
-            setActiveMenu(true)
-        }
-    }, [screenSize])
+  useEffect(() => {
+    if (screenSize < 768) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
 
-    return (
-        <div className="nav-container">
-            <div className="logo-container">
-                <Avatar src={""} size="large" />
-                <Typography.Title level={2} className="logo">
-                    <Link to="/">Cryptoverse</Link>
-                    {/* <Button className="menu-control-container"></Button> */}
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={Digcoin} size="large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Digcoin</Link>
+          {/* <Button className="menu-control-container"></Button> */}
+        </Typography.Title>
+        <Button
+          className="menu-control-container"
+          onClick={() => setActiveMenu(!activeMenu)}
+        >
+          <MenuOutlined />
+        </Button>
+      </div>
+      {activeMenu && (
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      )}
+    </div>
+  );
+};
 
-                </Typography.Title>
-                <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
-            </div>
-            {activeMenu &&
-                <Menu theme="dark">
-                <Menu.Item icon={<HomeOutlined />}>
-                    <Link to="/">Home</Link>
-                </Menu.Item>
-                <Menu.Item icon={<FundOutlined />}>
-                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-                </Menu.Item>
-                <Menu.Item icon={<MoneyCollectOutlined />}>
-                    <Link to="/exchanges">Exchanges</Link>
-                </Menu.Item>
-                <Menu.Item icon={<BulbOutlined />}>
-                    <Link to="/news">News</Link>
-                </Menu.Item>
-            </Menu>
-            }
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
