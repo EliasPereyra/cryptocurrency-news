f2c4f5f583810e7133b44ce557a346fb78680a48
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-05-05 17:24:21 -0300

    Update the index with new changes made in the components folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,2 @@
 export { default as Navbar } from './Navbar'
-export { default as Cryptocurrencies } from './Cryptocurrencies'
-export { default as CryptoDetails } from './CryptoDetails'
-export { default as Exchanges } from './Exchanges'
-export { default as Homepage } from './Homepage'
-export { default as News } from './News'
 export { default as DualRingSpinner } from './DualRingSpinner'
